d99ee50307f9790c3e386fcdaddabe9751394e9c
kwklly <yeonlee@i.ewha.ac.kr>
2021-11-07 04:07:42 +0900

    #3 add MuiButton theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,15 +1,15 @@
-import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
-
+import { responsiveFontSizes } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
 import palette from "./palette";
 
 const theme = responsiveFontSizes(
-  createMuiTheme({
+  createTheme({
     palette,
     layout: {
       contentWidth: 1140,
     },
     typography: {
-      fontFamily: "Lato",
+      fontFamily: "SpaceMono",
     },
     zIndex: {
       appBar: 1200,
@@ -18,10 +18,24 @@ const theme = responsiveFontSizes(
     overrides: {
       MuiButton: {
         contained: {
+          boxShadow: "0 0 0",
+          color: "#846cff",
+          border: "solid 1px #846cff",
           backgroundColor: "white",
+          "&:hover": {
+            boxShadow: "0 0 0",
+            backgroundColor: "#846cff",
+            color: "white",
+          },
         },
         containedSecondary: {
+          color: "white",
           backgroundColor: "#846cff",
+          "&:hover": {
+            border: "solid 1px #846cff",
+            backgroundColor: "white",
+            color: "#846cff",
+          },
         },
       },
     },
